0331f9e6fe8aec8c768c178d5c51da43b55b7154
ullizen <andersson.au@gmail.com>
2017-09-20 21:27:14 +0200

    Footer stuff

--- src/Header/Title.jsx
+++ src/Header/Title.jsx
@@ -5,7 +5,7 @@ class Header extends Component {
     return (
     	<div id="title">
     	<i className="fa fa-magic fa-2x"></i>
-      	<h2>live-wiki leaks <span id="underscore">_</span></h2>
+      	<h2>live-wiki leaks <span id="underscor">_</span></h2>
       </div>
       )
   }
